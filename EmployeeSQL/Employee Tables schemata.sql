6cb90d09b25bbe5b2020b47bd894318e53f9caa6
YoussoufIY <youssouf.ismael88@gmail.com>
2023-01-05 19:56:08 -0500

    adding updated schemata file

--- EmployeeSQL/Employee Tables schemata.sql
+++ EmployeeSQL/Employee Tables schemata.sql
@@ -2,30 +2,30 @@
 
 --Create titles table
 CREATE TABLE titles(
-title_id VARCHAR PRIMARY KEY NOT NULL,
-title VARCHAR);
+title_id VARCHAR(20) PRIMARY KEY NOT NULL,
+title VARCHAR(20));
 
 --Create employees table
 CREATE TABLE employees(
 emp_no INT PRIMARY KEY NOT NULL,
 emp_title_id VARCHAR NOT NULL,
-birth_date VARCHAR NOT NULL,
-first_name VARCHAR NOT NULL,
-last_name VARCHAR NOT NULL,
-sex VARCHAR NOT NULL,
-hire_date VARCHAR NOT NULL,
+birth_date VARCHAR(10) NOT NULL,
+first_name VARCHAR(15) NOT NULL,
+last_name VARCHAR(15) NOT NULL,
+sex VARCHAR(4) NOT NULL,
+hire_date VARCHAR(10) NOT NULL,
 FOREIGN KEY (emp_title_id) REFERENCES titles(title_id));
 
 --Create salaries table
 CREATE TABLE salaries(
 emp_no INT,
-salary INT NOT NULL,
+salary INT(15) NOT NULL,
 FOREIGN KEY (emp_no) REFERENCES employees(emp_no));
 
 --Create departments table
 CREATE TABLE departments(
-dept_no VARCHAR PRIMARY KEY NOT NULL,
-dept_name VARCHAR NOT NULL);
+dept_no VARCHAR(10) PRIMARY KEY NOT NULL,
+dept_name VARCHAR(15) NOT NULL);
 
 --Create dept_emp table
 CREATE TABLE dept_emp(
